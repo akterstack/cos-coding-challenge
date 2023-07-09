888c31a68abb2d54540ce1344464d93649e7d53d
akter <akterstack@gmail.com>
2023-07-10 00:32:50 +0200

    handle empty array for average calc

--- challenges/backend/src/app/helpers/utils.spec.ts
+++ challenges/backend/src/app/helpers/utils.spec.ts
@@ -3,6 +3,13 @@ import { calcAverage } from './utils';
 
 describe('Helper/util functions', () => {
   describe('#calcAverage', () => {
+    it('should return 0 for empty number array.', () => {
+      const sampleNumbers: number[] = [];
+      expect(calcAverage(sampleNumbers)).to.be.equal(0);
+    });
+  });
+
+  describe('#calcAverage', () => {
     it('should calculate average of numbers provided.', () => {
       const sampleNumbers = [4, 6, 7, 5, 8];
       expect(calcAverage(sampleNumbers)).to.be.equal(6);
--- challenges/backend/src/app/helpers/utils.ts
+++ challenges/backend/src/app/helpers/utils.ts
@@ -1,2 +1,7 @@
-export const calcAverage = (items: number[]) =>
-  items.reduce((sum, item) => sum + item) / items.length;
+export const calcAverage = (items: number[]) => {
+  if (!items || items.length === 0) {
+    return 0;
+  }
+
+  return items.reduce((sum, item) => sum + item) / items.length;
+};
